b89df292a63ddfb42a9106e6ec8ce1c7bd910724
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-11-24 17:38:52 +0530

    Base

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/2.10.1/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ipriyam26/FlutterProjects/search_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/ipriyam26/FlutterProjects/search_app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "EXTRA_FRONT_END_OPTIONS=--no-sound-null-safety"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/ipriyam26/FlutterProjects/search_app/.dart_tool/package_config.json"
---
+++ lib/main.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/route_manager.dart';
+import 'package:here_sdk/core.dart';
+import 'package:here_sdk/core.engine.dart';
+import 'package:here_sdk/core.errors.dart';
+import 'package:here_sdk/mapview.dart';
+import 'env.dart';
+import 'SearchExample.dart';
+
+void main() {
+  // Usually, you need to initialize the HERE SDK only once during the lifetime of an application.
+  _initializeHERESDK();
+
+  // Ensure that all widgets, including MyApp, have a MaterialLocalizations object available.
+  runApp(ScreenUtilInit(
+    designSize: const Size(360, 800),
+    minTextAdapt: true,
+    splitScreenMode: true,
+    builder: (context, child) {
+      return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        home: MyApp(),
+      );
+    },
+  )
+
+      // GetMaterialApp(home: MyApp())
+
+      );
+}
+
+void _initializeHERESDK() async {
+  // Needs to be called before accessing SDKOptions to load necessary libraries.
+  SdkContext.init(IsolateOrigin.main);
+
+  // Set your credentials for the HERE SDK.
+  String accessKeyId = Secrets().accessKeyId;
+  String accessKeySecret = Secrets().accessKeySecret;
+  SDKOptions sdkOptions =
+      SDKOptions.withAccessKeySecret(accessKeyId, accessKeySecret);
+
+  try {
+    await SDKNativeEngine.makeSharedInstance(sdkOptions);
+  } on InstantiationException {
+    throw Exception("Failed to initialize the HERE SDK.");
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  SearchExample? _searchExample;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          HereMap(onMapCreated: _onMapCreated),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                height: 40.h,
+                // width: 34.w,
+                margin: EdgeInsets.only(left: 10.w, right: 10.w, top: 60.h),
+                child: TextField(
+                  textAlignVertical: TextAlignVertical.center,
+                  scrollPadding: EdgeInsets.zero,
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.zero,
+                    filled: true,
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Color(0xff9CA3AF),
+                    ),
+                    fillColor: Colors.white,
+                    hintText: 'What are you looking for?',
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: new BorderSide(color: Colors.white),
+                      borderRadius: new BorderRadius.circular(11),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: new BorderSide(color: Colors.white),
+                      borderRadius: new BorderRadius.circular(11),
+                    ),
+                  ),
+                  onSubmitted: (value) {
+                    // _searchExample!.search(value);
+                  },
+                ),
+              ),
+              // button('Search', _searchButtonClicked),
+              // button('Geocoding', _geocodeAnAddressButtonClicked),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onMapCreated(HereMapController hereMapController) {
+    hereMapController.mapScene.loadSceneForMapScheme(MapScheme.normalDay,
+        (MapError? error) {
+      if (error == null) {
+        _searchExample = SearchExample(_showDialog, hereMapController);
+      } else {
+        print("Map scene not loaded. MapError: " + error.toString());
+      }
+    });
+  }
+
+  void _searchButtonClicked() {
+    _searchExample?.searchButtonClicked();
+  }
+
+  void _geocodeAnAddressButtonClicked() {
+    _searchExample?.geocodeAnAddressButtonClicked();
+  }
+
+  @override
+  void dispose() {
+    // Free HERE SDK resources before the application shuts down.
+    SDKNativeEngine.sharedInstance?.dispose();
+    SdkContext.release();
+    super.dispose();
+  }
+
+  // A helper method to add a button on top of the HERE map.
+  Align button(String buttonLabel, Function callbackFunction) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.lightBlueAccent,
+          onPrimary: Colors.white,
+        ),
+        onPressed: () => callbackFunction(),
+        child: Text(buttonLabel, style: TextStyle(fontSize: 20)),
+      ),
+    );
+  }
+
+  // A helper method to show a dialog.
+  Future<void> _showDialog(String title, String message) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(message),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
