4d7b5425282da1f0b7008043975b96a9f11e516d
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-11-24 18:57:23 +0530

    added plugins

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -1,35 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:multi_select_flutter/bottom_sheet/multi_select_bottom_sheet.dart';
 
 class BottomOpener extends StatelessWidget {
-  const BottomOpener({Key? key}) : super(key: key);
+  BottomOpener({Key? key}) : super(key: key);
+  var _selectedActivities = [];
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 700.h,
-      width: 360.w,
-      padding: EdgeInsets.only(top: 14.h),
-      // color: Colors.red,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(30),
-          topRight: Radius.circular(30),
+        height: 700.h,
+        width: 360.w,
+        padding: EdgeInsets.only(top: 14.h),
+        // color: Colors.red,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          color: Colors.white,
         ),
-        color: Colors.white,
+        // color: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 106.w,
+              height: 6.h,
+              decoration: BoxDecoration(
+                  color: Color(0xffD9D9D9),
+                  borderRadius: BorderRadius.circular(8)),
+            ),
+            MultiSelectActivities()
+          ],
+        ));
+  }
+}
 
-      ),
-      // color: Colors.white,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-      Container(width: 106.w,
-    height: 6.h,
-    decoration: BoxDecoration(
-    color: Color(0xffD9D9D9),
-      borderRadius: BorderRadius.circular(8)),
-      )
-      ],)
+class MultiSelectActivities extends StatefulWidget {
+  const MultiSelectActivities({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MultiSelectActivities> createState() => _MultiSelectActivitiesState();
+}
+
+class _MultiSelectActivitiesState extends State<MultiSelectActivities> {
+  var selected = [];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // width: 181.w,
+      child: Wrap(children: [
+        Actitvity()
+      ],),
+    );
+  }
+}
+
+class Actitvity extends StatelessWidget {
+  const Actitvity({
+    Key? key,
+
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ClipOval(
+          child: Container(
+            color: Color(0xff3AA766),
+            padding: EdgeInsets.all(2),
+            child: ClipOval(child: Container(
+              color: Colors.white,
+              child: Icon(Icons.local_gas_station_rounded,size: 36.w,),
+width: 60.w,
+height: 60.h,
+            )),
+          ),
+        ),
+        Text("")
+      ],
     );
   }
 }
